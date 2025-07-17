f65af5df11fa6307238bdebf8880ec4f37c84168
romane <romane.valenti@gmail.com>
2025-07-17 18:37:52 +0200

    Mise en place de la gateway et spring security

--- gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
@@ -2,12 +2,24 @@ package com.medilabo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("patients", r -> r.path("/api/patients", "/api/patients/**")
+						// Pas de rewritePath ici, on envoie le path complet au backend
+						.uri("http://localhost:8081"))
+				.build();
 	}
 
 }
---
+++ gateway/src/main/java/com/medilabo/gateway/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.medilabo.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/patients/**").authenticated()
+                        .anyExchange().permitAll()
+                )
+                .httpBasic()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("password123")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+}
